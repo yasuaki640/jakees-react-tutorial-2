adfe188823aa6adc621634380cbf27fcba48ef06
yasuaki640 <yasuaki640@gmail.com>
2022-07-06 15:29:58 +0900

    molecules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { PrimaryButton } from './components/atoms/buttom/PrimaryButton';
 import './styles.css';
 import { SecondaryButton } from './components/atoms/buttom/SecondaryButton';
+import { SearchInput } from './components/molecules/SearchInput';
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
