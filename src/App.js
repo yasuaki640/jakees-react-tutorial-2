208e8f6b2e732a2f0ca7ea08f9d5a95c74f807ea
yasuaki640 <yasuaki640@gmail.com>
2022-07-06 14:18:17 +0900

    init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { PrimaryButton } from "./components/atoms/buttom/PrimaryButton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+    </div>
+  );
+}
---
+++ src/components/atoms/buttom/SecondaryButton.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PrimaryButton = (props) => {
+  const { children } = props;
+  return <SButton>{children}</SButton>;
+};
+
+const SButton = styled.button`
+  background-color: #40514e;
+  color: #fff;
+  padding: 6px 24px;
+`;
