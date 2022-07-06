8a4b064b535116dcd3282c6f6e4f4815c9e1b561
yasuaki640 <yasuaki640@gmail.com>
2022-07-06 18:02:22 +0900

    as;ofijewif

---
+++ src/components/atoms/layout/Footer.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Footer = () => {
+  return (
+    <SFooter>
+      &copy; 2022 yasuaki640
+    </SFooter>
+  );
+};
+
+const SFooter = styled.footer`
+  background-color: #11999e;
+  color: #fff;
+  text-align: center;
+  padding: 8px 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
