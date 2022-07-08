05dc3b740b6f932a0ac09a6e3f9cc7bf7509311b
yasuaki640 <yasuaki640@gmail.com>
2022-07-08 18:27:24 +0900

    finishssusheswoieu

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,43 @@
 import styled from 'styled-components';
+import { SearchInput } from '../molecules/SearchInput';
+import { UserCard } from '../orgamisms/user/UserCard';
+
+const users = [...Array(10).keys()].map((val) => ({
+  id: val,
+  name: `jakee${val}`,
+  image: 'https://source.unsplash.com/cZAj2txBFO8',
+  email: `$(photo.user.name}@test.com`,
+  phone: Math.random().toString(),
+  company: {
+    name: 'test co.',
+  },
+  website: 'https://unsplash.com/photos/cZAj2txBFO8',
+}));
 
 export const Users = () => {
   return (
     <SContainer>
-      <h2>This is a smashing users list.</h2>
+      <h2>Incredible users list.</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
