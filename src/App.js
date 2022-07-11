381304353e2c5209918a10a0d4c1982c0b7556de
yasuaki640 <yasuaki640@gmail.com>
2022-07-11 11:25:55 +0900

    destroy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './styles.css';
-import { Router } from './router/Router';
-import { UserProvider } from './providers/UserProvider';
-import { RecoilRoot } from 'recoil';
 
 export default function App() {
   return (
-    <RecoilRoot>
-      <UserProvider>
-        <Router />
-      </UserProvider>
-    </RecoilRoot>
+    <div className='App'>
+      <h1>Destroyed.</h1>
+    </div>
   );
 }
