07bf874b21eef16131ec7505b38296135984027c
yasuaki-watanabe <yasuaki-watanabe@dip-net.co.jp>
2022-07-12 14:01:53 +0900

    kansei

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './styles.css';
+import axios from 'axios';
 
 export default function App() {
+  const onClickUsers = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((r) => {
+        console.log(r.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+  const onClickUser1 = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users/1')
+      .then((r) => {
+        console.log(r.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
   return (
     <div className='App'>
-      <h1>Destroyed.</h1>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id 1 users</button>
     </div>
   );
 }
